b085501364f8a33e2e8e4226b6cc91e752f7c58c
Singlerr <singlerr@soongsil.ac.kr>
2022-08-04 13:47:31 +0900

    Updated build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,40 @@
 plugins {
     id("java")
+    id("maven-publish")
     id("com.github.breadmoirai.github-release") version "2.4.1"
 }
 
+val author = "Singlerr"
+
 group = "io.github.singlerr"
-version = "1.0.0"
+version = "1.0.4"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
 
-githubRelease{
+java{
+    withSourcesJar()
+    withJavadocJar()
+}
 
+
+githubRelease{
+    token { System.getenv("GITHUB_TOKEN") }
+    owner.set(author)
+    repo.set(rootProject.name)
+    tagName.set("v${version}")
+    targetCommitish.set("master")
+    releaseName.set("v${version}")
+    generateReleaseNotes.set(true)
+    releaseAssets.setFrom(tasks.jar.get().destinationDirectory.asFile.get().listFiles())
 }
